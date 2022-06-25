da69191d94ab444e3166c16f388d39148a34ec83
Boluwatife <omotayodaniel10@gmail.com>
2022-06-25 08:49:51 +0100

    More changes

--- script.js
+++ script.js
@@ -23,3 +23,13 @@ function outsideClick(e) {
     closeModal();
   }
 }
+
+const showModal = function (e) {
+  modal.classList.toggle("active");
+
+  if (modal.classList.contains("active")) {
+    body.style.overflow = "hidden";
+  } else {
+    body.style.overflow = "auto";
+  }
+};
